4bedfc4db7626182d5a5cd436922836cc3a84399
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-08-12 13:14:50 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiply = void 0;
+var multiply = function (x, b) {
+    return x * b;
+};
+exports.multiply = multiply;
+console.log(multiply(10, 20));
---
+++ main.ts
@@ -0,0 +1,7 @@
+const multiply = (x: number, b:number): number => {
+    return x * b;
+}
+
+console.log(multiply(10, 20))
+
+export { multiply }
